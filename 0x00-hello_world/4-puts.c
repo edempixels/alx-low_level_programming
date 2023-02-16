0f06283e4d090ae628cedfa97058fc4d72694319
edempixels <info@edempixels.com>
2023-02-16 14:21:14 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - this is the main function
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle"):
-        return (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
