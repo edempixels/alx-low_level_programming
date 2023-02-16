abd78eab59b56207fab71edb7d012617a7b84823
edempixels <info@edempixels.com>
2023-02-16 14:47:24 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main functin of this programme
  *
- * Return: always 0 
+ * Return: always 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
