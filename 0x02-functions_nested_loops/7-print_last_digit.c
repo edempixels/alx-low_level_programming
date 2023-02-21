6831c4e2846d165f833d76cd758c42a1f35c0ed1
edempixels <info@edempixels.com>
2023-02-21 15:25:05 -0800

    3 10 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,14 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int a;
 
 	if (n < 0)
 	{
 		n = -n;
 	}
+		a = n % 10;
+	_putchar(a + '0');
 
-	last_digit = n % 10;
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+	return (a);
 }
