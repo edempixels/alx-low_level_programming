b94e5b3a5720b67064e05f2de4b710d8df096a9b
edempixels <info@edempixels.com>
2023-02-27 15:52:36 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: integer value of string
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int num = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = -sign;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = num * 10 + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (num * sign);
+}
