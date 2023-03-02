2516fb798ad1312e72ff3fc1356c7641739f0900
edempixels <info@edempixels.com>
2023-03-02 06:06:15 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: difference between ASCII values of first non-matching characters,
+ * or 0 if the strings are equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+	}
+
+	return (s1[i] - s2[i]);
+}
