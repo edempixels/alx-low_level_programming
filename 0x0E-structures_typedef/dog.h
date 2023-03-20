083508c902788b2ee3d98c248b9d9c80fb224df2
edempixels <info@edempixels.com>
2023-03-20 08:21:10 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's attributes
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
