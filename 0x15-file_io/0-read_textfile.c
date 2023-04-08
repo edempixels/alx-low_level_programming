9efb02f2c8d2e94cfcfe219af78ff02921034b9e
edempixels <info@edempixels.com>
2023-04-08 06:56:06 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to stdout
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: The number of letters it could read and print, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+
+	return (n_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+# 0x15. C - File I/O
+0. Tread lightly, she is near
+1. Under the snow
+2. Speak gently, she can hear
+3. cp Write a program that copies the content of a file to another file.
